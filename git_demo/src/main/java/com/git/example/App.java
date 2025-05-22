cf318154bda89250a19722f53dc08ce41f3ef2d0
Nivek <email@mail.ru>
2025-05-22 15:28:52 +0500

    Добавил строку для вывода в консоль

--- git_demo/src/main/java/com/git/example/App.java
+++ git_demo/src/main/java/com/git/example/App.java
@@ -9,6 +9,7 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "Hello World!, it's me" );
-        
+        System.out.println("Я добавил что-то новое");
     }
 }
+св
